a746165b9877a7cc9b7d8d12af5977dabfa96565
just856 <justicen246@gmail.com>
2025-04-03 11:27:50 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "JOEL~XMD~WZj0KrAR",
+  SESSION_ID:JOEL-XMD~lFIgVKJD#L7LJn7iY0bIZ0bbiNvCtgDU_eSEwGzUfpClDIfz9-2A process.env.SESSION_ID || "JOEL~XMD~WZj0KrAR",
   PREFIX: process.env.PREFIX || '.',
 //let's add menu captions by lord joel
    BOT_NAME: process.env.BOT_NAME || "ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ ᴠ¹⁰",
