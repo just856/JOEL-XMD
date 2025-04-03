0ffcf6ac538f13488d3d552e5bcee228f1c1a46b
just856 <justicen246@gmail.com>
2025-04-03 11:36:58 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID:JOEL-XMD~lFIgVKJD#L7LJn7iY0bIZ0bbiNvCtgDU_eSEwGzUfpClDIfz9-2A process.env.SESSION_ID || "JOEL~XMD~WZj0KrAR",
+  SESSION_ID: process.env.SESSION_ID || JOEL-XMD~lFIgVKJD#L7LJn7iY0bIZ0bbiNvCtgDU_eSEwGzUfpClDIfz9-2A
   PREFIX: process.env.PREFIX || '.',
 //let's add menu captions by lord joel
    BOT_NAME: process.env.BOT_NAME || "ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ ᴠ¹⁰",
@@ -42,7 +42,7 @@ const config = {
   DELETED_MESSAGES_CHAT_ID: process.env.DELETED_MESSAGES_CHAT_ID || "255781144539@s.whatsapp.net",
   OWNER_NAME: process.env.OWNER_NAME || "ʟᴏʀᴅ ᴊᴏᴇʟ",
   /* ['255781144539@s.whatsapp.net', '255714595078@s.whatsapp.net']*/
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "255714595078",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "27682679605",
   SUDO_NUMBER: process.env.SUDO_NUMBER || "255781144539",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
